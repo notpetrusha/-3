6fa5301d5d76403b1fcd8d1a23fe12c62d7e9d62
notpetrusha <113039866+notpetrusha@users.noreply.github.com>
2023-03-27 17:25:16 +0300

    Add files via upload

--- sdacha.py
+++ sdacha.py
@@ -1,17 +1,18 @@
 N=int(input('Введите сдачу '))
-monetki={int(input('Номинал ')):int(input('Количество денег данного номинала ')), int(input('Номинал ')):int(input('Количество денег данного номинала ')),
-         int(input('Номинал ')):int(input('Количество денег данного номинала ')), int(input('Номинал ')):int(input('Количество денег данного номинала '))}
+monetki={int(input('Номинал ')):int(input('Количество денег номинала ')), int(input('Номинал ')):int(input('Количество денег номинала ')),
+         int(input('Номинал ')):int(input('Количество денег номинала ')), int(input('Номинал ')):int(input('Количество денег номинала '))}
 monetki_sdacha={}
 
 while N!=0:
     nominal= max(monetki.keys())
     n=N//nominal
-    if monetki[nominal]<=n:
-        N-=nominal*monetki[nominal]
-        monetki_sdacha[nominal]=monetki[nominal]
-    else:
-        N-=n*monetki[nominal]
-        monetki_sdacha[nominal] = n
+    if n>0:
+        if monetki[nominal]<=n:
+            N-=nominal*monetki[nominal]
+            monetki_sdacha[nominal]=monetki[nominal]
+        else:
+            N-=n*nominal
+            monetki_sdacha[nominal] = n
     monetki.pop(nominal)
 print(monetki_sdacha)
 
